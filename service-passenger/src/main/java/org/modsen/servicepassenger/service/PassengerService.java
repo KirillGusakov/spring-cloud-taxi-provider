006c7ddffa57c639b118f8af82f766410a3f2e13
Kirill <justmymail0003@gmail.com>
2024-09-09 14:00:26 +0300

    remove files

--- service-passenger/src/main/java/org/modsen/servicepassenger/service/PassengerService.java
+++ service-passenger/src/main/java/org/modsen/servicepassenger/service/PassengerService.java
@@ -5,7 +5,6 @@ import org.modsen.servicepassenger.dto.response.PassengerResponseDto;
 import org.springframework.data.domain.Pageable;
 import java.util.List;
 
-
 public interface PassengerService {
     PassengerResponseDto findById(Long id);
 
