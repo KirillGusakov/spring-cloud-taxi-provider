104c6e79463d530e7e7eff0562a410a9feb303cf
Kirill <justmymail0003@gmail.com>
2024-10-15 17:21:58 +0300

    add eureka server

--- service-rating/src/main/java/org/modsen/servicerating/ServiceRatingApplication.java
+++ service-rating/src/main/java/org/modsen/servicerating/ServiceRatingApplication.java
@@ -2,10 +2,12 @@ package org.modsen.servicerating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class ServiceRatingApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceRatingApplication.class, args);
